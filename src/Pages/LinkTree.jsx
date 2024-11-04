b141d35cb50eff8aadcec00c73dde0cf368936dd
Matt Engerer <matt.engerer@aol.com>
2024-11-03 19:28:27 -0500

    added more links, changed styling

--- src/Pages/LinkTree.jsx
+++ src/Pages/LinkTree.jsx
@@ -3,12 +3,12 @@ import {Container, Header, SubHeader, Link, LinkList, LinkItem, Description } fr
 function LinkTree() {
     return (
       <Container>
-                <Header>Brian Fog Shit。</Header>
+                <Header>brian fog drum & noise &</Header>
 
 
 <SubHeader>*SPECIAL*</SubHeader>
         <LinkList>
-        nothing here yet
+        nothing here
           {/* <LinkItem>
             <Link href="https://www.youtube.com/watch?v=1TUQ1Fus6qo&ab_channel=MechForestSound">battle milk 1st show - live at Wenona Lodge (Track Could Bend Number 87)</Link>
          <Description>Thank you Joe!</Description>
@@ -32,6 +32,7 @@ function LinkTree() {
             <Link href="https://www.instagram.com/mite.hole/">Brian Fog Instagram</Link>
           </LinkItem>
           <LinkItem>
+          
             <Link href="https://www.youtube.com/watch?v=K9uhxRw5sWc&ab_channel=Dddddd"> Brian Fog - live at Bar Orwell March 28 2024 </Link>
             <Description>thx to Blash.</Description>
           </LinkItem>
@@ -44,6 +45,35 @@ function LinkTree() {
                 Brian Ruryk, Density Creep (1/2 of battle milk) and Brian Fog live on More Noise Please Met Radio CJRU.ca</Link>
           </LinkItem>
           <Description>Thank u SuperJen!</Description>
+        </LinkList>
+
+        <SubHeader>Postictal</SubHeader>
+
+        <LinkList>
+          <LinkItem>
+            <Link href="https://www.instagram.com/postictal__/">Postictal Instagrm</Link>
+         <Description> electronic proj slowly becoming something .</Description>
+          </LinkItem>
+        </LinkList>
+
+
+        <SubHeader>Battle Milk</SubHeader>
+        <LinkList>
+          <LinkItem>
+            <Link href="https://www.youtube.com/watch?v=1TUQ1Fus6qo&ab_channel=MechForestSound">battle milk 1st show - live at Wenona Lodge (Track Could Bend Number 87)</Link>
+         <Description>Thank you Joe!</Description>
+          </LinkItem>
+          <LinkItem>
+            <Link href="https://www.youtube.com/watch?v=hG4b4gXPIbk&ab_channel=AndrewP.">battle milk 2nd show - live at Tranzac Living Room </Link>
+            <Description>Happy Bday Philip - thx Andrew)) </Description>
+          </LinkItem>
+          <LinkItem>
+            <Link href="https://youtu.be/J0ILaFKOKEM?si=03bnwMk3qnrIBB7p.">battle milk 4th show - live at Tranzac Living Room </Link>
+            <Description>thx Andrew)) </Description>
+          </LinkItem>
+          <LinkItem>
+            <Link href="https://battlemilk.bandcamp.com">battle milk Bandcamp</Link>
+          </LinkItem>
         </LinkList>
         
         <SubHeader>Lava Gland</SubHeader>
@@ -69,31 +99,14 @@ function LinkTree() {
           <Description>Thank u Blash Belmar for all LG vids</Description>
         </LinkList>
 
-        <SubHeader>Battle Milk</SubHeader>
-        <LinkList>
-          <LinkItem>
-            <Link href="https://www.youtube.com/watch?v=1TUQ1Fus6qo&ab_channel=MechForestSound">battle milk 1st show - live at Wenona Lodge (Track Could Bend Number 87)</Link>
-         <Description>Thank you Joe!</Description>
-          </LinkItem>
-          <LinkItem>
-            <Link href="https://www.youtube.com/watch?v=hG4b4gXPIbk&ab_channel=AndrewP.">battle milk 2nd show - live at Tranzac Living Room </Link>
-            <Description>Happy Bday Philip - thx Andrew)) </Description>
-          </LinkItem>
-          <LinkItem>
-            <Link href="https://battlemilk.bandcamp.com">battle milk Bandcamp -coming</Link>
-          </LinkItem>
-        </LinkList>
+      
 
         <SubHeader>Mite </SubHeader>
         <LinkList>
           <LinkItem>
             <Link href="https://mitehole.bandcamp.com/">Mite bandcamp</Link>
             <Description>MY other, older solo project</Description>
-          </LinkItem>
-          <LinkItem>
-            <Link href="https://mitehole.netlify.app/">MITE OFFICIAL WEBSITE</Link>
-            <Description>With Year 1999 Image Loading Times</Description>
-          </LinkItem>
+          </LinkItem>            
           {/* <LinkItem>
             <Link href="#">Link 3</Link>
           </LinkItem> */}
@@ -130,14 +143,14 @@ function LinkTree() {
         <LinkList>
           <LinkItem>
             <Link href="https://bonecrate.bandcamp.com/album/the-wrestling-noise-compiliation-vol-2">Bone Crate - Wrestling Noise Compilation</Link>
-          <Description>feat. BRIAN FOG - EXTICTRANFM  ... OH!</Description>
+          <Description>feat. BRIAN FOG - EXTICTRANFM </Description>
           </LinkItem>
-          <LinkItem>
+          {/* <LinkItem>
             <Link href="#">Link 2</Link>
           </LinkItem>
           <LinkItem>
             <Link href="#">Link 3</Link>
-          </LinkItem>
+          </LinkItem> */}
         </LinkList>
       </Container>
     );
--- src/Pages/LinkTree.styles.jsx
+++ src/Pages/LinkTree.styles.jsx
@@ -6,6 +6,7 @@ export const Container = styled.div`
   margin: 0 auto;
   padding: 20px;
   text-align: center;
+  border: 1px dotted white;
   @media (max-width: 600px) {
   /* Adjust container width for smaller screens */
     max-width: 90%;
